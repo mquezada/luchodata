9ff14e2ae3da04f50426b1147cd979a487d709cf
Mauricio Quezada <waxkun@gmail.com>
2017-11-26 21:30:39 -0300

    asdf

--- main.py
+++ main.py
@@ -49,9 +49,6 @@ with codecs.open('luchodata.txt', 'a', encoding='utf-8') as f:
                 if u'¿sabes qué es big data?' in new_text.lower() and random.random() < 0.9:
                     return
 
-                if new_text.startswith("@"):
-                    new_text = '.' + new_text
-
                 new_text = re.sub(r'&amp;', r'&', new_text)
 
                 new_text = re.sub(r'(\bel\b) ' + BIG_DATA, r'\2', new_text, flags=re.IGNORECASE)
@@ -78,16 +75,13 @@ with codecs.open('luchodata.txt', 'a', encoding='utf-8') as f:
                 new_text = re.sub(r'#machinelearning', u'#MuchoGusto', new_text, flags=re.IGNORECASE)
                 new_text = re.sub(IA, u'Paty Maldonado', new_text, flags=re.IGNORECASE)
 
-
                 logging.info(new_text)
 
                 if len(new_text) <= 280:
                     try:
-                        if triggers(new_text) or random.random() >= 0.7:
-                            api.update_status(new_text)
-                            f.write(new_text + '\n')
-                        else:
-                            logging.info("Last message not triggered")
+                        api.update_status(new_text)
+                        f.write(new_text + '\n')
+
                     except tweepy.TweepError as e:
                         if e.api_code == 187:
                             return
